43d269eeea4f3904a7fdec54d0e0e580c8b2b9b7
Anton Taran <antondp23@gmail.com>
2022-11-10 18:17:10 +0200

    add files

--- js/task-07.js
+++
@@ -1,8 +0,0 @@
-const rangeInput = document.querySelector ('#font-size-control');
-const spanText = document.querySelector('span#text');
-
-rangeInput.addEventListener('change', onInputRangeChange);
-
-function onInputRangeChange(event) {
-    spanText.style.fontSize = `${event.currentTarget.value}px`;
-}
